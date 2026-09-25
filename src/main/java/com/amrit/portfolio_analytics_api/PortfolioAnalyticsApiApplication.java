90fc96133729f0a038b8a791db5e9d7cd62990e5
jitamrit <amritjitsinghvirdi@gmail.com>
2026-09-25 17:36:46 +0100

    feat: initial commit with Spring Boot 3 skeleton, project layout, and health check

---
+++ src/main/java/com/amrit/portfolio_analytics_api/PortfolioAnalyticsApiApplication.java
@@ -0,0 +1,13 @@
+package com.amrit.portfolio_analytics_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioAnalyticsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioAnalyticsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amrit/portfolio_analytics_api/PortfolioAnalyticsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amrit.portfolio_analytics_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioAnalyticsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
